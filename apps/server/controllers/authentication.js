738364f27e15e7d617107646e33d2a2f110156c0
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2025-06-12 16:37:02 -0400

    feat(auth-routes) Add auth routes and methods.

--- apps/server/controllers/authentication.js
+++ apps/server/controllers/authentication.js
@@ -3,25 +3,52 @@ const User = require('../models/user');
 const keys = require('../config/keys');
 
 const tokenForUser = (user) => {
-	const timestamp = Math.round(Date.now() / 1000);
-	return jwt.encode(
-		{
-			sub: user.id,
-			iat: timestamp,
-			exp: timestamp + 5 * 60 * 60,
-		},
-		keys.TOKEN_SECRET
-	);
+  const timestamp = Math.round(Date.now() / 1000);
+  return jwt.encode(
+    {
+      sub: user.id,
+      iat: timestamp,
+      exp: timestamp + 5 * 60 * 60,
+    },
+    keys.TOKEN_SECRET
+  );
 };
 
 exports.signin = (req, res) => {
-	// TODO
+  res.send({ email: req.user.email, token: tokenForUser(req.user) });
 };
 
 exports.currentUser = (req, res) => {
-	// TODO
+  const user = {
+    email: req.user.email,
+    token: tokenForUser(req.user),
+  };
+  res.send(user);
 };
 
 exports.signup = async (req, res, next) => {
-	// TODO
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide email and password' });
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+    const user = new User();
+    user.email = email;
+    user.setPassword(password);
+    await user.save();
+
+    res.json({ token: tokenForUser(user) });
+  } catch (error) {
+    next(err);
+  }
 };
--- apps/server/router.js
+++ apps/server/router.js
@@ -5,4 +5,9 @@ const passport = require('passport');
 const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
-module.exports = function (app) {};
+module.exports = function (app) {
+  app.post('/auth/signup', Authentication.signup);
+  app.post('/auth/signin', requireSignin, Authentication.signin);
+  app.get('/success', requireAuth, (req, res) => res.send('Successful login.'));
+  app.get('/auth/current_user', requireAuth, Authentication.currentUser);
+};
