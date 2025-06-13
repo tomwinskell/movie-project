57e3b631d57143db7edcec4e34cb388cefadf978
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2025-06-13 12:36:29 -0400

    refactor(watchlist-slice) Amend watchlist slice, abstract config to the top of file, reduce repetition. Add asyncthunk for patch remove movie from watchlist.

--- apps/web/app/store/slices/watchListSlice.js
+++ apps/web/app/store/slices/watchListSlice.js
@@ -3,76 +3,84 @@ import axios from 'axios';
 
 const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}`;
 const isServer = typeof window === 'undefined';
+const config = {
+  headers: {
+    Authorization: `Bearer ${!isServer ? localStorage.getItem('token') : ''}`,
+  },
+};
 
 // Async thunks
 export const addMovieToWatchList = createAsyncThunk(
-	'watchlist/addMovie',
-	async (movie, { rejectWithValue }) => {
-		try {
-			const config = {
-				headers: {
-					Authorization: `Bearer ${
-						!isServer ? localStorage.getItem('token') : ''
-					}`,
-				},
-			};
-			const response = await axios.post(
-				`${BASE_URL}/watchlist`,
-				{ movie },
-				config
-			);
-			return response.data;
-		} catch (error) {
-			return rejectWithValue(error.response.data);
-		}
-	}
+  'watchlist/addMovie',
+  async (movie, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        `${BASE_URL}/watchlist`,
+        { movie },
+        config
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
 );
 
 export const fetchWatchListMovies = createAsyncThunk(
-	'watchlist/fetchMovies',
-	async (_, { rejectWithValue }) => {
-		try {
-			const config = {
-				headers: {
-					Authorization: `Bearer ${
-						!isServer ? localStorage.getItem('token') : ''
-					}`,
-				},
-			};
-			const response = await axios.get(`${BASE_URL}/watchlist`, config);
-			return response.data;
-		} catch (error) {
-			return rejectWithValue(error.response.data);
-		}
-	}
+  'watchlist/fetchMovies',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(`${BASE_URL}/watchlist`, config);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const removeMovieFromWatchList = createAsyncThunk(
+  'watchlist/removeMovie',
+  async (movie, { rejectWithValue }) => {
+    try {
+      const response = await axios.patch(
+        `${BASE_URL}/watchlist`,
+        { movie },
+        config
+      );
+
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
 );
 
 // Slice
 const watchlistSlice = createSlice({
-	name: 'watchlist',
-	initialState: {
-		movies: [],
-		count: 0,
-		status: 'idle',
-		error: null,
-	},
-	reducers: {},
-	extraReducers: (builder) => {
-		builder
-			.addCase(addMovieToWatchList.fulfilled, (state, action) => {
-				state.movies.push(action.payload);
-			})
-			.addCase(fetchWatchListMovies.fulfilled, (state, action) => {
-				state.movies = action.payload.movies;
-				state.count = action.payload.watchListCount;
-			})
-			.addCase(addMovieToWatchList.rejected, (state, action) => {
-				state.error = action.error.message;
-			})
-			.addCase(fetchWatchListMovies.rejected, (state, action) => {
-				state.error = action.error.message;
-			});
-	},
+  name: 'watchlist',
+  initialState: {
+    movies: [],
+    count: 0,
+    status: 'idle',
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchWatchListMovies.fulfilled, (state, action) => {
+        state.movies = action.payload.movies;
+        state.count = action.payload.watchListCount;
+      })
+      .addCase(addMovieToWatchList.rejected, (state, action) => {
+        state.error = action.error.message;
+      })
+      .addCase(fetchWatchListMovies.rejected, (state, action) => {
+        state.error = action.error.message;
+      })
+      .addCase(removeMovieFromWatchList.rejected, (state, action) => {
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default watchlistSlice.reducer;
