de063fe21529436e9ca2736c0c7ca2840e5921b0
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2025-06-13 14:55:34 -0400

    hotfix(authorization) Move config into method for authorization for watchlist.

--- apps/web/app/store/slices/watchListSlice.js
+++ apps/web/app/store/slices/watchListSlice.js
@@ -3,17 +3,19 @@ import axios from 'axios';
 
 const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}`;
 const isServer = typeof window === 'undefined';
-const config = {
-  headers: {
-    Authorization: `Bearer ${!isServer ? localStorage.getItem('token') : ''}`,
-  },
-};
 
 // Async thunks
 export const addMovieToWatchList = createAsyncThunk(
   'watchlist/addMovie',
   async (movie, { rejectWithValue }) => {
     try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${
+            !isServer ? localStorage.getItem('token') : ''
+          }`,
+        },
+      };
       const response = await axios.post(
         `${BASE_URL}/watchlist`,
         { movie },
@@ -30,6 +32,13 @@ export const fetchWatchListMovies = createAsyncThunk(
   'watchlist/fetchMovies',
   async (_, { rejectWithValue }) => {
     try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${
+            !isServer ? localStorage.getItem('token') : ''
+          }`,
+        },
+      };
       const response = await axios.get(`${BASE_URL}/watchlist`, config);
       return response.data;
     } catch (error) {
@@ -42,6 +51,13 @@ export const removeMovieFromWatchList = createAsyncThunk(
   'watchlist/removeMovie',
   async (movie, { rejectWithValue }) => {
     try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${
+            !isServer ? localStorage.getItem('token') : ''
+          }`,
+        },
+      };
       const response = await axios.patch(
         `${BASE_URL}/watchlist`,
         { movie },
