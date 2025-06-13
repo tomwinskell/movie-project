0d1eacaa7d720a47d2e1039cb89f213f91964a59
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2025-06-13 12:35:26 -0400

    refactor(watchlist-button): Amend watchlist button, adding remove button if movie is on watchlist.

--- apps/web/app/components/WatchListButton.js
+++ apps/web/app/components/WatchListButton.js
@@ -2,27 +2,45 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useRouter } from 'next/navigation';
-import { addMovieToWatchList } from '../store/slices/watchListSlice';
+import {
+  addMovieToWatchList,
+  removeMovieFromWatchList,
+} from '../store/slices/watchListSlice';
+import useWatchListMovies from '../hooks/useWatchListMovies';
 
 const WatchListButton = ({ movie }) => {
-	const dispatch = useDispatch();
-	const router = useRouter();
-	const authenticated = useSelector((state) => state.auth.authenticated);
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const authenticated = useSelector((state) => state.auth.authenticated);
+  const { watchListMovies } = useWatchListMovies();
 
-	if (authenticated) {
-		return (
-			<button
-				onClick={() => {
-					dispatch(addMovieToWatchList(movie));
-					router.push('/watch-list');
-				}}
-			>
-				Add to Watchlist
-			</button>
-		);
-	} else {
-		return null;
-	}
+  const watchlistIncludesMovie = watchListMovies.some(
+    (movieOnList) => movieOnList.id === movie.id
+  );
+
+  if (authenticated) {
+    return watchlistIncludesMovie ? (
+      <button
+        onClick={() => {
+          dispatch(removeMovieFromWatchList(movie));
+          router.push('/watch-list');
+        }}
+      >
+        Remove from Watchlist
+      </button>
+    ) : (
+      <button
+        onClick={() => {
+          dispatch(addMovieToWatchList(movie));
+          router.push('/watch-list');
+        }}
+      >
+        Add to Watchlist
+      </button>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default WatchListButton;
